9a0c85344f5a2f900fe99b6b01d01e69c1edac98
caerulex <timelesswind@gmail.com>
2024-02-02 19:46:00 -0800

    Formatting.

--- doc_proofreader/__main__.py
+++ doc_proofreader/__main__.py
@@ -11,4 +11,6 @@ OUTPUT_DIR = Path(Path(__file__).parent.parent, "proofread_files")
 if __name__ == "__main__":
     args = parser.parse_args()
     print("Processing ", args.file_path, args.additional_instructions)
-    proofread_document(args.file_path, args.additional_instructions, save_dir=OUTPUT_DIR)
+    proofread_document(
+        args.file_path, args.additional_instructions, save_dir=OUTPUT_DIR
+    )
--- doc_proofreader/cli.py
+++ doc_proofreader/cli.py
@@ -5,7 +5,9 @@
 import argparse
 
 parser = argparse.ArgumentParser(
-    prog="Doc-Proofreader", description="Proofreads documents with AI!", epilog="A simple app by caerulex."
+    prog="Doc-Proofreader",
+    description="Proofreads documents with AI!",
+    epilog="A simple app by caerulex.",
 )
-parser.add_argument("file_path", metavar='file-path')
+parser.add_argument("file_path", metavar="file-path")
 parser.add_argument("--additional-instructions", type=str, default="")
